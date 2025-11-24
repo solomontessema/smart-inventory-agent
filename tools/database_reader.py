5b0842971e4778f0e1c3ceccf8cce872dc172401
solomon-tessema <solomon.tessema@outlook.com>
2025-11-23 22:33:02 -0600

    directories and place holder files.

--- tools/database_reader.py
+++ tools/database_reader.py
@@ -1,14 +1,1 @@
-# tools/database_reader.py
-
-
-def _exec_sql() -> str:
-    return (
-        "We have the following inventory\n"
-        "Item Name|Barcode|Quantity|Threshold\n"
-        "Coca Cola|2344330012|200|120\n"
-        "Pepsi|450023144|60|110"
-    )
-
-def read_database_tool(input: str) -> str:
-   return  _exec_sql()
-    
+# database reader
